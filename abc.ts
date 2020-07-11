70a47352721efe80b1b8469ae6e1523f5c832736
thoriqdharmawan <thoriqd21@gmail.com>
2020-07-12 01:03:56 +0700

    read static files

--- abc.ts
+++ abc.ts
@@ -2,6 +2,9 @@ import { Application, Context } from "https://deno.land/x/abc@v1/mod.ts";
 
 const app = new Application()
 
+// static files
+app.static('/', './public')
+
 // Routes
 app.get("/", async (ctx: Context) => {
   await ctx.file('./public/index.html')
